0cfc09322c7e1fe0ae3ff0b4137d56d8c0f0bde4
Rakesh Pulipati <rakesh.pulipati@gmail.com>
2014-03-17 00:25:58 +0530

    Changed project version

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject dynamodb_explorer "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject dynamodb_explorer "0.2.0"
+  :description "Webapp to explore dynamodb"
+  :url ""
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.6"]
 
